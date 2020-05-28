0f3a359413858e09115c1d4fcff2ff3e8221c204
HALOGENMAN <Shayak.malakar.159@gmail.com>
2020-05-28 16:43:55 +0530

    style

---
+++ src/pages/homepage/homePage.components.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homePage.style.scss";
+import Directory from "../../components/directory/directory.component"
+const HomePage = () =>
+    (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+    
+
+
+export default  HomePage;
